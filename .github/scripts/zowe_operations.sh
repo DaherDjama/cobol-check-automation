ece63b7a786b1b1c0f18a301cd275a74c2c866ce
Djama Ahmed Daher <djamaahmeddaher@gmail.com>
2026-09-17 21:18:57 +0300

    Fix zowe_operations.sh to use explicit profile 

--- .github/scripts/zowe_operations.sh
+++ .github/scripts/zowe_operations.sh
@@ -1,20 +1,20 @@
-#!/bin/bash
+"#!/bin/bash
 # zowe_operations.sh
 
 # Convert username to lowercase
 LOWERCASE_USERNAME=$(echo "$ZOWE_USERNAME" | tr '[:upper:]' '[:lower:]')
 
 # Check if directory exists, create if it doesn't
-if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" &>/dev/null; then
+if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" --zosmf-profile zosmf &>/dev/null; then
     echo "Directory does not exist. Creating it..."
-    zowe zos-files create uss-directory /z/$LOWERCASE_USERNAME/cobolcheck
+    zowe zos-files create uss-directory "/z/$LOWERCASE_USERNAME/cobolcheck" --zosmf-profile zosmf
 else
     echo "Directory already exists."
 fi
 
 # Upload files
-zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --recursive --binary-files "cobol-check-0.2.19.jar"
+zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --recursive --binary-files "cobol-check-0.2.19.jar" --zosmf-profile zosmf
 
 # Verify upload
 echo "Verifying upload:"
-zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck"
+zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" --zosmf-profile zosmf
